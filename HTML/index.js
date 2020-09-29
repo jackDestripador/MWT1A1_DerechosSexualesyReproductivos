f2979a2061eb0a03d7b3b32a50396ec174e8267c
Jesus Cisneros <chucho.cisneros@hotmail.com>
2020-09-29 01:09:24 -0500

    configuracion del servidor nodejs

--- HTML/index.js
+++ HTML/index.js
@@ -1,20 +1,8 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
 
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-
-  res.end('Hello World\n');
-  
-});
-
-
-
-
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname)); 
+app.listen('3000', function() {
+  console.log('Servidor web escuchando en el puerto 3000');
+})
